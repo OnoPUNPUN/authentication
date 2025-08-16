89da851145a8a33b6460575b3bcd9a49be5530f8
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-08-16 10:23:56 +0600

    update the .gitignore

--- lib/screens/log_in_screen.dart
+++ lib/screens/log_in_screen.dart
@@ -56,121 +56,123 @@ class _LogInScreenState extends State<LogInScreen> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              // logo
-              Icon(Icons.lock, size: 100),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                // logo
+                Icon(Icons.lock, size: 100),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              // welcome back, you've been missed!
-              Text(
-                "Welcome back your've been missed",
-                style: TextStyle(color: Colors.grey[700], fontSize: 16),
-              ),
+                // welcome back, you've been missed!
+                Text(
+                  "Welcome back your've been missed",
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 25),
 
-              //username textfield
-              MyTextfield(
-                controller: _emailTEcontroller,
-                hintText: 'Email',
-                obscureText: false,
-              ),
+                //username textfield
+                MyTextfield(
+                  controller: _emailTEcontroller,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // password textfield
-              MyTextfield(
-                controller: _passwordTEcontroller,
-                hintText: 'Passowrd',
-                obscureText: true,
-              ),
+                // password textfield
+                MyTextfield(
+                  controller: _passwordTEcontroller,
+                  hintText: 'Passowrd',
+                  obscureText: true,
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // forgot password?
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                // forgot password?
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // sing in button
+                MyButton(onTap: singUserIn),
+
+                const SizedBox(height: 50),
+
+                // or continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                // google + apple sing in buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //google button
+                    SqureTile(imagePath: 'images/logo/google-logo.svg'),
+
+                    const SizedBox(width: 10),
+
+                    //apple button
+                    SqureTile(imagePath: 'images/logo/apple-logo.svg'),
+                  ],
+                ),
+
+                const SizedBox(height: 50),
+
+                // not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
+                      'Not a member?',
+                      style: TextStyle(color: Colors.grey[700]),
                     ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // sing in button
-              MyButton(onTap: singUserIn),
-
-              const SizedBox(height: 50),
-
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
+                    const SizedBox(width: 4),
+                    const Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Expanded(
-                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
-                    ),
                   ],
                 ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // google + apple sing in buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //google button
-                  SqureTile(imagePath: 'images/logo/google-logo.svg'),
-
-                  const SizedBox(width: 10),
-
-                  //apple button
-                  SqureTile(imagePath: 'images/logo/apple-logo.svg'),
-                ],
-              ),
-
-              const SizedBox(height: 50),
-
-              // not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    'Register now',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
